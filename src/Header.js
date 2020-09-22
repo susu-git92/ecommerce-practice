143b32ce9f5ca25a7b3f155f43afce0c0d526a93
susu-git92 <chlojeong@gamil.com>
2020-09-23 03:53:37 +0900

    styling css

--- src/Header.js
+++ src/Header.js
@@ -2,13 +2,14 @@ import React from 'react';
 import './Header.css';
 import {Link} from 'react-router-dom';
 import { IoLogoOctocat } from 'react-icons/io';
-import SearchIcon from "@material-ui/icons/Search"
+import SearchIcon from "@material-ui/icons/Search";
+import { BiBasket} from 'react-icons/bi';
 
 function Header() {
   return (
     <nav className="header">
       <Link to="/login">
-      <IoLogoOctocat className="header_logo" alt="logo"/>
+      <IoLogoOctocat style={{ color: '#fff' }} className="header_logo" alt="logo"/>
       </Link>
 
       {/*Search box*/ }
@@ -16,27 +17,39 @@ function Header() {
         <input type="text" className="header_searchInput" />
         <SearchIcon className="header_searchIcon" />
       </div>
+
+
        {/*3Links*/ }
        <div className="headerNav">
 
          <Link to="/login" className="header_link">
             <div className="header_option">
-              <span>Hello Qazi</span>
-              <span>Sign In</span>
+              <span className="header_optionLineOne">Hello Qazi</span>
+              <span className="header_optionLineTwo">Sign In</span>
             </div>
          </Link>
 
          <Link to="/login"  className="header_link">
             <div className="header_option">
-              <span>Returns</span>
-              <span>& Orders</span>
+              <span className="header_optionLineOne">Returns</span>
+              <span className="header_optionLineTwo">& Orders</span>
             </div>
          </Link>
 
          <Link to="/login"  className="header_link">
             <div className="header_option">
-              <span>Your</span>
-              <span>Prime</span>
+              <span className="header_optionLineOne" >Your</span>
+              <span className="header_optionLineTwo">Prime</span>
+            </div>
+         </Link>
+
+         <Link to="/checkout">
+            <div className="header_optionBasket">
+               {/*shopping basket con*/ }
+               <BiBasket  style={{ color: '#fff' }} />
+
+               {/*Number of items in the basket*/ }
+               <span>0</span>
             </div>
          </Link>
 
